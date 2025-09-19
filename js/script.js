17f8c35c4dcac70f8169fd9fe252d86cf86dfc89
ahmadghiffaryr <ahmadghiffaryr1@gmail.com>
2025-09-19 23:49:50 +0700

    Refactor To-Do List display to use a table format and enhance task management with complete and delete functionality

--- js/script.js
+++ js/script.js
@@ -24,20 +24,48 @@ function addTodo() {
 
 function displayTodos() {
     /// clear existing list
-    const todoList = document.getElementById("todo-list");
+    const todoList = document.getElementById("todo-table-body");
     todoList.innerHTML = "";
 
     /// display all todos
     todos.forEach((todo, _) => {
         todoList.innerHTML += `
-            <li class="border-b border-gray-300 py-2">${todo.task} - <span
-                        class="text-sm text-gray-500">${todo.date}</span>
-        </li>`;
+            <tr>
+                <td class="border px-4 py-2">${todo.task}</td>
+                <td class="border px-4 py-2">${todo.date}</td>
+                <td class="border px-4 py-2">${todo.status}</td>
+                <td class="border px-4 py-2">
+                    <button class="bg-green-500 text-white px-2 py-1 rounded mr-2" onclick="completeTodo()">Complete</button>
+                    <button class="bg-red-500 text-white px-2 py-1 rounded" onclick="deleteTodo()">Delete</button>
+                </td>
+            </tr>
+        `;
     });
 }
 
 /// mandatory functions
-function deleteTodo() { }
+function deleteTodo() {
+    try {
+        // Assuming the index of the todo to delete is passed as an argument
+        const index = event.target.getAttribute('data-index');
+        todos.splice(index, 1);
+        displayTodos();
+    } catch (error) {
+        console.error("Error deleting todo:", error);
+    }
+}
+
+function completeTodo() {
+    try {
+        if (event.target) {
+            const index = event.target.getAttribute('data-index');
+            todos[index].status = "Complete";
+            displayTodos();
+        }
+    } catch (error) {
+        console.error("Error completing todo:", error);
+    }
+}
 
 /// mandatory functions
 function filterTodos() { }
